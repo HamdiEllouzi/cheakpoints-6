aceb05ca3eea4064c8333a137e2aa0f417a9362a
HamdiEllouzi <hlouzi089@gmail.com>
2021-10-17 18:11:51 +0100

    cheakpoints6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+const style = {
+  display:'flex',
+  flexDirection:'column',
+  justifyContent:'center',
+  alignItems:'center'
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      Person:{ 
+        fullName:'hamdi',
+        bio:'random text', 
+        imgSrc: logo,
+        profession:'web dev'},
+        show:false,
+        count: 0
+    }
+  }
+  componentDidMount() {
+     setInterval(
+      () => this.count(),
+      1000
+    );
+  }
+  count() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+stateViewToggle = () => this.setState({show:!this.state.show})
+  render() {
+    const {fullName,bio,imgSrc,profession} = this.state.Person
+    const {show,count}= this.state
+    return <div style={style}>
+      <h2>It is {count}s</h2>
+      {(show && (
+        <div style={style}>
+          <img src={imgSrc} alt='logo' className='App-logo'/>
+          <span>{fullName}</span>
+          <span>{profession}</span>
+          <span>{bio}</span>
+        </div>
+      ))}
+      <button onClick={this.stateViewToggle}>{(!show)?'Show State':'hide State'}</button>
     </div>
-  );
+  }
 }
 
 export default App;
